4f2f35739b883880bf293e7df58d44089615e60b
Mathias Aschhoff <web@little-devil.de>
2016-10-17 11:50:03 +0200

    Add files via upload

--- src/dev/ztgnrw/HL72PDF.java
+++ src/dev/ztgnrw/HL72PDF.java
@@ -14,11 +14,13 @@ import javax.xml.transform.stream.StreamSource;
 
 /**
  *
- * @author web
+ * @author Mathias Aschhoff ZTG 2016 m.aschhoff@ztg-nrw.de
  */
 public class HL72PDF {
 
     /**
+     * Main method for CommandLine options it needs 3 args
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -26,13 +28,22 @@ public class HL72PDF {
         if (args.length < 3) {
             System.err.println("Usage HL72PDF [cda] [stylesheet] [output filename]");
             return;
-        }
+        };
 
-        String cda_file, stylesheet, output_file;
-        cda_file = args[0];
-        stylesheet = args[1];
-        output_file = args[2];
+        convertToPDF(args[0], args[1], args[2]);
 
+    }
+
+    /**
+     * This method converts a xml with xslt stylesheet to PDF As Params you have
+     * 3 Strings the CDA filepath, the stylesheet path and the filename for the
+     * output file.
+     *
+     * @param cda_file CDA filepath
+     * @param stylesheet XSLT filepath
+     * @param output_file filename for outputfile
+     */
+    public static void convertToPDF(String cda_file, String stylesheet, String output_file) {
         try {
             System.out.println("************* Stating the Transformation *************");
             //XML to HTML          
@@ -41,14 +52,12 @@ public class HL72PDF {
             StringWriter writer = new StringWriter();
             StreamResult result = new StreamResult(writer);
             transformer.transform(new StreamSource(cda_file), result);
-            String html = writer.toString();
             //HTML TO PDF with Attachment
-            HtmlConverter.fromStringToPDF(html,output_file,cda_file);  
-            System.out.println("************* The result is in "+output_file+".pdf *************");
+            HtmlConverter.fromStringToPDF(writer.toString(), output_file, cda_file);
+            System.out.println("************* The result is in " + output_file + ".pdf *************");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
 }
